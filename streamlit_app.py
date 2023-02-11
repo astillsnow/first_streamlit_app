634fcc86240ccb839d522df4d7d0863a2e2772d4
astillsnow <123436573+astillsnow@users.noreply.github.com>
2023-02-11 12:06:17 -0700

    Update streamlit_app.py
    Added requests library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 streamlit.title('My Parents\' New Healthy Diner')
-  
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
